cc1aa248953f0ec96dae575b74feb0eb0d80450f
blmeue <2981885528@qq.com>
2024-03-23 11:29:25 +0800

    第十五届蓝桥杯嵌入式模拟赛第一场

---
+++ practice/15th_moni1/Bsp/myadc.c
@@ -0,0 +1,13 @@
+#include "myadc.h"
+
+double Get_Adc(ADC_HandleTypeDef *pin)
+{
+    int value;
+    
+    HAL_ADC_Start(pin);
+    
+    value=HAL_ADC_GetValue(pin);
+    
+    return value*(3.3/4096);
+    
+}
